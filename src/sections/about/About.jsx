887c9c721fdf44ea5d854f24f8f7f73dbcde4a4f
crisleyhguimaraes <crisleyhguimaraes@gmail.com>
2023-10-13 15:43:55 -0300

    termino da estrutura principal do projeto

---
+++ src/sections/about/About.jsx
@@ -0,0 +1,11 @@
+import "./about.css";
+
+const About = () => {
+    return (
+        <section id="about">
+            <h2>Abouts</h2>
+        </section>
+    );
+};
+
+export default About;
---
+++ src/sections/portfolio/Portfolio.jsx
@@ -0,0 +1,11 @@
+import "./portfolio.css";
+
+const Portfolio = () => {
+    return (
+        <sectiom id="portfolio">
+            <h2>Portfolio</h2>
+        </sectiom>
+    );
+};
+
+export default Portfolio;
