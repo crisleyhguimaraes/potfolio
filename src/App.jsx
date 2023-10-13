fe3ba6ba28a5f53997aecaf47d418558e5acc9a5
crisleyhguimaraes <crisleyhguimaraes@gmail.com>
2023-10-13 17:08:42 -0300

    criacao de variaveis e estilos css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Navbar from "./sections/navbar/Navbar";
+import Header from "./sections/header/Header";
+import Portfolio from "./sections/portfolio/Portfolio";
+import About from "./sections/about/About";
+import Services from "./sections/services/Services";
+import Testimonials from "./sections/testimonials/Testimonials";
+import FAQs from "./sections/faqs/FAQs";
+import Contact from "./sections/contact/Contact";
+import Footer from "./sections/footer/Footer";
+import FloatingNav from "./sections/floating-nav/FloatingNav";
 
 const App = () => {
-    return <div>teste</div>;
+    return (
+        <main>
+            <Navbar />
+            <Header />
+            <About />
+            <Services />
+            <Portfolio />
+            <Testimonials />
+            <FAQs />
+            <Contact />
+            <Footer />
+            <FloatingNav />
+        </main>
+    );
 };
 
 export default App;
